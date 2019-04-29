9cb63585961cc758cbc6f47b2206c419ebbe51b4
hegemonies <hegemonies@yandex.ru>
2019-04-29 13:13:02 +0700

    Fix

--- src/main/java/LJ/Parser/Parser.java
+++ src/main/java/LJ/Parser/Parser.java
@@ -51,8 +51,56 @@ public class Parser {
     }
 
     private void parseInit() throws CriticalProductionException {
-        listLexer.matchOneOf("int", "char"); // native data type
-        
+        parseNativeDataType();
+        listLexer.match("id");
+        parseForkInit();
+    }
+
+    private void parseNativeDataType() throws CriticalProductionException {
+        listLexer.matchOneOf("int", "char");
+    }
+
+    private void parseForkInit() { //todo refactor
+        parseForkInitFunc();
+        parseForkInitVar();
+        parseForkInitArray();
+    }
+
+    private boolean parseForkInitFunc() {
+        try {
+            listLexer.match("l_paren");
+            try {
+                parseArgsInitListChanger();
+            } catch (OptionalProductionException e) { }
+            listLexer.match("r_paren");
+            listLexer.match("l_brace");
+            parseStatementList();
+            listLexer.match("r_brace");
+        } catch (CriticalProductionException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void parseStatementList() {
+        // todo statementList
+    }
+
+    private void parseArgsInitListChanger() throws OptionalProductionException {
+        parseArgsInitList();
+    }
+
+    private void parseArgsInitList() {
+        // todo argsInitList
+    }
+
+    private boolean parseForkInitVar() {
+        // todo forkInitVar
+    }
+
+    private boolean parseForkInitArray() {
+        // todo forInitArray
     }
 
     private void parseMainMethod() throws CriticalProductionException {
